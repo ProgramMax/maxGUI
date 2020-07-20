b748ff53be7ee4101fd543a63d704376081251b5
Chris Blume <ProgramMax@gmail.com>
2020-07-20 03:05:39 -0700

    Foo

---
+++ Code/maxGUI/Rectangle.cpp
@@ -0,0 +1,18 @@
+// Copyright 2020, The maxGUI Contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <maxGUI/Rectangle.hpp>
+
+#include <utility>
+
+namespace maxGUI {
+
+	Rectangle::Rectangle(int top, int left, int height, int width) MAX_DOES_NOT_THROW
+		: top_(std::move(top))
+		, left_(std::move(left))
+		, height_(std::move(height))
+		, width_(std::move(width))
+	{}
+
+} // namespace maxGUI
