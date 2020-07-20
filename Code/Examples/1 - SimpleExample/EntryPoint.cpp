d24135f0c899567409f4a15c25e8469ca5717000
Chris Blume <ProgramMax@gmail.com>
2020-07-20 05:04:37 -0700

    Templatize factories
    Right now, users need to create a new derived factory class for
    forms/controls any time they want to make a change, including where a
    control gets placed on a form. If there are two of the same type of
    control on a form, each needs its own factory.
    
    The real goal of the factories is to instantiate a derived form/control.
    This same goal can be achieved using templated factories.
    
    This commit templatizes the factories to allow typical code to be easier
    to read & write.

--- Code/Examples/1 - SimpleExample/EntryPoint.cpp
+++ Code/Examples/1 - SimpleExample/EntryPoint.cpp
@@ -20,7 +20,7 @@ public:
 
 	void OnCreated() MAX_DOES_NOT_THROW override {
 		// Add controls inside the Form's OnCreated().
-		maxGUI::MultilineTextBoxFactory multiline_textbox_factory(maxGUI::Rectangle(0, 0, 100, 100), "Test");
+		maxGUI::MultilineTextBoxFactory<> multiline_textbox_factory(maxGUI::Rectangle(0, 0, 100, 100), "Multi-line\r\ntest");
 		multiline_textbox_ = AddControl(&multiline_textbox_factory);
 	}
 
@@ -35,23 +35,12 @@ public:
 
 };
 
-// Create a custom form factory that can allocate our custom form.
-class CustomFormFactory : public maxGUI::FormFactory {
-public:
-
-	// TODO: Can we not need to know about HWND somehow?
-	std::unique_ptr<maxGUI::Form> AllocateForm(HWND window_handle) MAX_DOES_NOT_THROW override {
-		return std::make_unique<CustomForm>(window_handle);
-	}
-
-};
-
 // Name your function EXACTLY this, keep it in the global namespace, and make sure it has MAX_DOES_NOT_THROW
 // If you get a linker error of an unresolved external symbol for maxGUIEntryPoint(), you did this wrong.
 int maxGUIEntryPoint(maxGUI::FormContainer&& form_container) MAX_DOES_NOT_THROW {
 	// Create the form
-	CustomFormFactory form_factory;
-	if (!form_container.CreateForm(form_factory)) {
+	maxGUI::FormFactory<CustomForm> custom_form_factory;
+	if (!form_container.CreateForm(custom_form_factory, 400, 600, "Simple Example")) {
 		return -1;
 	}
 	// IMPORTANT: Keep the form factory alive long enough for the form to be created by the message pump
